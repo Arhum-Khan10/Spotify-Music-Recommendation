10bebbd6c08ef24b0cbf4a5a2b8241ec1e65b1ce
Arhum Khan <i221967@nu.edu.pk>
2024-05-12 12:27:11 +0500

    Added CSS.

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ def loading():
 def landing():
     return render_template('landing.html')
 
+@app.route("/index")
+def index():
+	return render_template("index.html")
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
